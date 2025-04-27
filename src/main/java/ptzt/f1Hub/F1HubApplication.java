699cac5bdebecfb36ef18278245a07b11c8af0b6
LermaOmar <omarlerma2004@gmail.com>
2025-04-27 11:36:48 +0200

    Commit inicial

---
+++ src/main/java/ptzt/f1Hub/F1HubApplication.java
@@ -0,0 +1,13 @@
+package ptzt.f1Hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1HubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1HubApplication.class, args);
+	}
+
+}
