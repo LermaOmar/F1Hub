b722acf0e5d56e35cbcdf22b4d5a4e07979be159
LermaOmar <omarlerma2004@gmail.com>
2025-05-06 14:40:03 +0200

    Creación de mercados por cada liga creada, con los respectivos items comunes a todas las ligas y definiendo los items mostrados en el mercado en ese momento

--- src/main/java/ptzt/f1Hub/application/services/league/LeagueServiceImpl.java
+++ src/main/java/ptzt/f1Hub/application/services/league/LeagueServiceImpl.java
@@ -1,31 +1,56 @@
 package ptzt.f1Hub.application.services.league;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ptzt.f1Hub.application.services.lineUp.LineUpService;
+import ptzt.f1Hub.application.services.market.MarketService;
+import ptzt.f1Hub.application.services.market.item.MarketItemService;
 import ptzt.f1Hub.domain.exceptions.EntityNotFoundException;
 import ptzt.f1Hub.domain.models.League;
 import ptzt.f1Hub.domain.models.LineUp;
+import ptzt.f1Hub.domain.models.market.Market;
+import ptzt.f1Hub.domain.models.market.MarketItem;
 import ptzt.f1Hub.instraestructure.repository.LeagueRepository;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
+@RequiredArgsConstructor(onConstructor_ = @Lazy)
 public class LeagueServiceImpl implements LeagueService{
 
     private final LeagueRepository leagueRepository;
     private final LineUpService lineUpService;
+    private final MarketService marketService;
+    private final MarketItemService marketItemService;
 
     @Transactional
     @Override
     public League create(League league) {
 
-        return leagueRepository.save(league);
+        List<MarketItem> items = marketItemService.getAllByMarkets(marketService.getAll());
 
+        Market market = new Market();
+        market.setLeague(league);
+
+        Market entityMarket = marketService.create(market);
+
+        league.setMarket(entityMarket);
+
+
+        League entity = leagueRepository.save(league);
+
+        items.forEach(marketItem -> {
+            marketItem.getMarkets().add(entityMarket);
+            marketItemService.update(marketItem);
+            if (marketItem.getAvailable())
+                marketItemService.displayInMarket(marketItem, List.of(entityMarket));
+        });
+
+        return entity;
     }
 
     @Override
