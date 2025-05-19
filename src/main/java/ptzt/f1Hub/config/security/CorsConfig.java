8ea88aaeb13bb3b886489335e9cd653070eabafd
LermaOmar <omarlerma2004@gmail.com>
2025-05-19 18:59:58 +0200

    Configuración del CORS

---
+++ src/main/java/ptzt/f1Hub/config/security/CorsConfig.java
@@ -0,0 +1,24 @@
+package ptzt.f1Hub.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("https://lermaomar.github.io", "http://localhost:5173")
+                .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
+
+    }
+
+}
