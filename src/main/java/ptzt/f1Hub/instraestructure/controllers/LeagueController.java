8eddc982311d87ab215a114a53e563fc777ae362
LermaOmar <omarlerma2004@gmail.com>
2025-06-14 17:10:24 +0200

    Get by ID league

--- src/main/java/ptzt/f1Hub/instraestructure/controllers/LeagueController.java
+++ src/main/java/ptzt/f1Hub/instraestructure/controllers/LeagueController.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import ptzt.f1Hub.application.services.account.AccountService;
 import ptzt.f1Hub.application.services.appUser.AppUserService;
@@ -58,7 +57,17 @@ public class LeagueController {
 
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<LeagueOutDto> getById(@PathVariable Long id){
 
+
+        return ResponseEntity.ok(
+                leagueMapper.toOutDto(
+                        leagueService.getById(id)
+                )
+        );
+
+    }
 
     @PutMapping("/{id}")
     public ResponseEntity<LeagueOutDto> update(@PathVariable Long id , @Valid @RequestBody LeagueInDto leagueInDto){
